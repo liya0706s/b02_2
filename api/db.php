07d244e1defdd9fc1f0f77f38386d045a8feb35c
liya0706s <liya0706s@gmail.com>
2024-02-19 16:32:53 +0800

    feat:db檔和index back include db

--- api/db.php
+++ api/db.php
@@ -13,28 +13,33 @@ class DB
         $this->pdo = new PDO($this->dsn, 'root', '');
     }
 
-    protected function a2s($array)
+    function q($sql)
     {
-        foreach ($array as $key => $value) {
-            if ($key != 'id') {
-                $tmp[] = "`$key`='$value'";
-            }
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    private function a2s($array)
+    {
+        foreach ($array as $col => $value) {
+            $tmp[] = "`$col`='$value'";
         }
         return $tmp;
     }
 
     private function sql_all($sql, $array, $other)
     {
-        // 如果有設定資料表且不為空
+        // 如果有設定的資料表且不是空的
         if (isset($this->table) && !empty($this->table)) {
-            // 如果參數是陣列
+            // 參數是陣列
             if (is_array($array)) {
-                // 而且陣列不為空
+                // 參數是陣列且不是空的
                 if (!empty($array)) {
+                    // 將陣列轉成字串放在$tmp變數
                     $tmp = $this->a2s($array);
                     $sql .= " where " . join(" && ", $tmp);
                 }
             } else {
+                // 有資料表，裡面的參數不是陣列
                 $sql .= " $array";
             }
             $sql .= $other;
@@ -42,15 +47,28 @@ class DB
         }
     }
 
-    protected function math($math, $col, $array = '', $other = '')
+    function all($where = '', $other = '')
     {
-        $sql = "select $math($col) from $this->table";
-        $sql = $this->sql_all($sql, $array, $other);
+        $sql = "select * from `$this->table` ";
+        $sql = $this->sql_all($sql, $where, $other);
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
 
+    function count($where = '', $other = '')
+    {
+        $sql = "select count(*) from `$this->table` ";
+        $sql = $this->sql_all($sql, $where, $other);
         return $this->pdo->query($sql)->fetchColumn();
     }
 
-    function sum($col, $where = '', $other = '')
+    private function math($math, $col, $array = '', $other = '')
+    {
+        $sql = "select $math(`$col`) from `$this->table` ";
+        $sql = $this->sql_all($sql, $array, $other);
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+    function sum($col = '', $where = '', $other = '')
     {
         return $this->math('sum', $col, $where, $other);
     }
@@ -65,77 +83,66 @@ class DB
         return $this->math('min', $col, $where, $other);
     }
 
-    function count($where = '', $other = '')
-    {
-        $sql = "select count(*) from `$this->table`";
-        $sql = $this->sql_all($sql, $where, $other);
-        return $this->pdo->query($sql)->fetchColumn();
-    }
-
-    function all($where = '', $other = '')
-    {
-        $sql = "select * from $this->table ";
-        $sql = $this->sql_all($sql, $where, $other);
-        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    }
-
     function find($id)
     {
-        $sql = "select * from $this->table";
+        $sql = "select * from `$this->table` ";
         if (is_array($id)) {
             $tmp = $this->a2s($id);
             $sql .= " where " . join(" && ", $tmp);
         } else if (is_numeric($id)) {
             $sql .= " where `id`='$id'";
         }
-        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+        $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+        return $row;
     }
 
+    function del($id)
+    {
+        $sql = "delete from `$this->table` where ";
 
-    function del($id){
-        $sql = "delete from $this->table";
-
-        if(is_array($id)){
-            $tmp= $this->a2s($id);
-            $sql .= " where " .join (" && ", $tmp);
-        }else if (is_numeric($id)){
-            $sql .= " where `id`='$id'";
+        if (is_array($id)) {
+            $tmp = $this->a2s($id);
+            $sql .= join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " `id`='$id'";
         }
-        // 將sql句子帶進pdo的exec方法中
         return $this->pdo->exec($sql);
     }
 
-    function save($array){
-        // 如果有id代表是更新
-        if(isset($array['id'])){
+    function save($array)
+    {
+        if (isset($array['id'])) {
             $sql = "update `$this->table` set ";
-            if(!empty($array)){
-                $tmp=$this->a2s($array);
+            if (!empty($array)) {
+                $tmp = $this->a2s($array);
             }
-            $sql.=join(",",$tmp);
-            $sql.=" where `id`={$array['id']}";
-        }else{
-            $sql="(`".join("`,`", array_keys($array)) . "`)";
-            $val ="('".join("','", $array). "')";
+            $sql .= join(",", $tmp);
+            $sql .= " where `id`='{$array['id']}'";
+        } else {
+            $sql = "insert into `$this->table` ";
+            $cols = "(`" . join("`,`", array_keys($array)) . "`)";
+            $vals = "('" . join("','", $array) . "')";
+            $sql = $sql . $cols . " values " . $vals;
         }
         return $this->pdo->exec($sql);
     }
-
-    public function q($sql){
-        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    }
-    
 }
-// class DB 結束
+// class DB 到此
+
+function dd($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
 
 function to($url){
-    header("location:" .$url);
+    header("location:$url");
 }
 
-function dd($array){
-    echo "<pre>";
-print_r($array);
-    echo "<pre>";
-}
+$Total = new DB('total');
+$User= new DB('user');
+$News=new DB('news');
+$Que=new DB('que');
+$Log= new DB('log');
 
-?>
