fd7d8ceb84099a1abef1de6003bd20c7c2f86ec5
Angie <liya0706s@gmail.com>
2024-02-21 22:13:31 +0800

    fix: api/get_list.php typo--getNews少打s 還有忘了結尾的a標籤

--- api/get_list.php
+++ api/get_list.php
@@ -1,16 +1,15 @@
 <?php
-
 include_once "db.php";
 
 // 取得所有指定分類的文章
-$rows=$News->all(['type'=>$_GET['type'],'sh'=>1]);
+$rows = $News->all(['type' => $_GET['type'], 'sh' => 1]);
 
 // 使用迴圈印出title文字及a連結內容
-foreach($rows as $row){
+foreach ($rows as $row) {
     echo "<div>";
     // 在點擊文章列表title連結時的執行取得文章內容的函式，並傳入文章id
-    echo "<a href='Javascript:getNew({$row['id']})'>";
+    echo "<a href='Javascript:getNews({$row['id']})'>";
     echo $row['title'];
+    echo "</a>";
     echo "</div>";
-
 }
