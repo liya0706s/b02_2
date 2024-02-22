6c6e96041add989ad824a128b1667d82dc6f7d20
Angie <liya0706s@gmail.com>
2024-02-22 21:30:42 +0800

    feat:後台最新文章管理完成

--- back/news.php
+++ back/news.php
@@ -1,9 +1,9 @@
 <!-- 建立後台文章列表 及 分頁連結 -->
 <form action="./api/edit_news.php" method="post">
-    <table style="text-align:center">
+    <table style="width:100%;text-align:center">
         <tr>
             <td>編號</td>
-            <td>標題</td>
+            <td style="width:60%">標題</td>
             <td>顯示</td>
             <td>刪除</td>
         </tr>
@@ -21,19 +21,40 @@
         foreach ($rows as $idx => $row) {
         ?>
             <tr>
-                <td><?=$idx+1+$start;?></td>
-                <td><?=$row['title'];?></td>
+                <td><?= $idx + 1 + $start; ?></td>
+                <td><?= $row['title']; ?></td>
                 <!-- 用sh欄位代表顯示, 判斷勾選與否 -->
                 <td>
-                    <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
+                    <input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>>
                 </td>
                 <td>
-                    <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
-                    <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+                    <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
+                    <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
                 </td>
             </tr>
         <?php
         }
         ?>
-        </table>
+    </table>
+    <div class="ct">
+        <?php
+        // 現在頁數減一大於零代表還有上一頁
+        if ($now - 1 > 0) {
+            $prev = $now - 1;
+            echo "<a href='back.php?do=news&p=$prev'> < </a>";
+        }
+        // 設定變數i從一開始跑，每次跑一圈，不能超過總頁數
+        for ($i = 1; $i <= $pages; $i++) {
+            // 設定字型尺寸，目前頁數比較大
+            $size = ($i == $now) ? 'font-size:22px;' : 'font-size:16px';
+            echo "<a href='back.php?do=news&p=$i' style='$size'> $i </a>";
+        }
+        // 現在的頁數加一小於等於總頁數，代表還有下一頁
+        if ($now + 1 <= $pages) {
+            $next = $now + 1;
+            echo "<a href='back.php?do=news&p=$next'> > </a>";
+        }
+        ?>
+    </div>
+    <div class="ct"><input type="submit" value="修改確定"></div>
 </form>
